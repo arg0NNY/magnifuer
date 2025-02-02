067d584f68894e6555f3c38d2834705841d08fac
Oleg Kapranov <arg0nny@mail.ru>
2025-02-02 17:19:22 +0300

    style: add linting

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import standard from '@vue/eslint-config-standard'
+
+export default defineConfigWithVueTs(
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
+  standard,
+  {
+    ignores: ['dist']
+  }
+)
