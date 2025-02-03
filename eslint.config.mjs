9f0f705348a1e42a7e4d2f52f20eed7070785ba5
Oleg Kapranov <arg0nny@mail.ru>
2025-02-03 20:11:12 +0300

    chore: disable eslint for example

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,10 @@ export default defineConfigWithVueTs(
   vueTsConfigs.recommended,
   standard,
   {
-    ignores: ['dist']
+    ignores: [
+      'dist',
+      'example'
+    ]
   },
   {
     rules: {
